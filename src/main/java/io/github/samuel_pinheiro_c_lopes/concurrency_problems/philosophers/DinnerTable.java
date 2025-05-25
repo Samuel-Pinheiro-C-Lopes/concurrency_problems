87e1945b19caf40d1e8bae6adca92961754dd536
Samuel-Pinheiro-C-Lopes <samuelpinheirocoutinholopes321@gmail>
2025-05-24 23:39:54 -0300

    Philosophers concurrency problem solution created

---
+++ src/main/java/io/github/samuel_pinheiro_c_lopes/concurrency_problems/philosophers/DinnerTable.java
@@ -0,0 +1,17 @@
+package io.github.samuel_pinheiro_c_lopes.concurrency_problems.philosophers;
+
+import java.util.concurrent.Semaphore;
+
+public class DinnerTable {
+    public final Semaphore mutex;
+    public final Semaphore[] forks;
+    public final Semaphore seats;
+
+    public DinnerTable(int seatsQuantity) {
+        this.mutex = new Semaphore(1, true);
+        this.forks = new Semaphore[seatsQuantity + 1];
+        for (int i = 0; i < seatsQuantity + 1; i++)
+            this.forks[i] = new Semaphore(1, true);
+        this.seats = new Semaphore(seatsQuantity, true);
+    }
+}
---
+++ src/main/java/io/github/samuel_pinheiro_c_lopes/concurrency_problems/philosophers/Philosopher.java
@@ -0,0 +1,59 @@
+package io.github.samuel_pinheiro_c_lopes.concurrency_problems.philosophers;
+
+import java.lang.Runnable;
+import java.lang.InterruptedException;
+
+import java.util.Random;
+import java.util.concurrent.Semaphore;
+import java.util.concurrent.TimeUnit;
+
+public class Philosopher implements Runnable {
+    private int id;
+    private Semaphore mutex;
+    private Semaphore[] forks;
+    private Semaphore seats;
+    private volatile boolean running = true;
+    private Random random;
+
+    public Philosopher(int id, Semaphore mutex, Semaphore[] forks, Semaphore seats) {
+        this.random = new Random();
+        this.id = id;
+        this.mutex = mutex;
+        this.forks = forks;
+        this.seats = seats;
+    }
+
+    @Override
+    public void run() {
+        while (this.running) {
+            try {
+                this.seats.acquire();
+                this.think();
+                this.forks[this.id].acquire();
+                this.forks[(this.id + 1)%this.forks.length].acquire();
+                this.eat();
+                this.forks[this.id].release();
+                this.forks[(this.id + 1)%this.forks.length].release();
+                this.think();
+                this.seats.release();
+                this.think();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private void think() throws InterruptedException {
+        System.out.println(this.id + " is thinking.");
+        TimeUnit.MILLISECONDS.sleep(25 + this.random.nextInt(200));
+    }
+
+    private void eat() throws InterruptedException {
+        System.out.println(this.id + " is eating.");
+        TimeUnit.MILLISECONDS.sleep(25 + this.random.nextInt(200));
+    }
+
+    public void stop() {
+        this.running = false;
+    }
+}
